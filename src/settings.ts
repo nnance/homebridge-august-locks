3156c9ae3f45f2ac115c0fd8a9481c045fece002
Nick Nance <nance.nick@gmail.com>
2021-12-15 22:14:27 -0600

    rename plugin and repo

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'AugustLocks';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-august-ts';
+export const PLUGIN_NAME = 'homebridge-august-locks';

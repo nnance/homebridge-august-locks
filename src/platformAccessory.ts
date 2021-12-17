7bcb275fb2fb22bbea372f71d0c0bcb8ab4ae4fb
Nick Nance <nance.nick@gmail.com>
2021-12-16 22:06:10 -0600

    publish version 1.1.0

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -31,7 +31,7 @@ export class AugustSmartLockAccessory {
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, lock.name);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/LockMechanism
@@ -92,7 +92,7 @@ export class AugustSmartLockAccessory {
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
    *
-   * GET requests should return as fast as possbile. A long delay here will result in
+   * GET requests should return as fast as possible. A long delay here will result in
    * HomeKit being unresponsive and a bad user experience in general.
    *
    * If your device takes time to respond you should update the status of your device
